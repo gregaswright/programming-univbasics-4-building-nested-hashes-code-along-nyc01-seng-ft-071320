e98cc25aab14ba9a001bb280f29144616770d653
Gregory A.S. Wright <geslim@gmail.com>
2020-06-26 20:15:21 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,21 +10,25 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        :name,
-        :age
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        :name,
-        :age
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }

f8af631ac8a3ad6c403e2e080351b560c7b8fb57
Gregory A.S. Wright <geslim@gmail.com>
2020-06-26 20:06:10 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,16 +1,8 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :matriarch => {},
-       :patriarch => {},
-       :hero => {},
-       :hero_friends => [{}, {}]
      },
      :capulet => {
-       :matriarch => {},
-       :patriarch => {},
-       :heroine => {},
-       :heroine_friends => [{}, {}]
      }
    }
 end

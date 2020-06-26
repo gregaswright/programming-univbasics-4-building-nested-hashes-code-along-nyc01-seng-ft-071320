30f34c679446b5ac23c5fe45577a396e00235096
Gregory A.S. Wright <geslim@gmail.com>
2020-06-26 20:13:16 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,25 +2,25 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague", 
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague", 
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        :name, 
+        :name,
         :age
         },
       :matriarch => {
-        :name, 
+        :name,
         :age
         },
       :heroine => {
@@ -30,5 +30,5 @@ def third_challenge
    }
   }
 
-  
+
 end
